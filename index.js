f9cd6a81745ad7f839109aae9c0d81f4bb77baf4
Emma Vasylyk <emma.ovsiannikova91@gmail.com>
2021-12-20 12:04:37 +0200

    .

---
+++ index.js
@@ -0,0 +1,41 @@
+const buttonSubmit = document.getElementById("button-submit");
+buttonSubmit.addEventListener("click", func);
+let result;
+
+function func(e) {
+  e.preventDefault();
+  const number1 = Number(document.getElementById("number1").value);
+  const number2 = Number(document.getElementById("number2").value);
+  const operator = document.getElementById("operator").value;
+
+  switch (operator) {
+    case "+":
+      result = number1 + number2;
+      break;
+
+    case "-":
+      result = number1 - number2;
+      break;
+
+    case "*":
+      result = number1 * number2;
+      break;
+    case "/":
+      if (number2 === 0 || number1 === 0) {
+        return alert("На ноль делить нельзя!");
+      } else {
+        result = number1 / number2;
+      }
+      break;
+
+    default:
+      break;
+  }
+  document.getElementById("result").innerHTML = Math.round(result);
+  clear();
+}
+
+function clear() {
+  document.getElementById("number1").value = "";
+  document.getElementById("number2").value = "";
+}
