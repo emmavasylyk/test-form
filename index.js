6cb17633531dd505434ef23e2576bd9e2cb4c7ac
Emma Vasylyk <emma.ovsiannikova91@gmail.com>
2021-12-20 15:45:37 +0200

    add pop-up

--- index.js
+++ index.js
@@ -1,28 +1,28 @@
-const buttonSubmit = document.getElementById("button-submit");
-buttonSubmit.addEventListener("click", func);
+const buttonSubmit = document.getElementById('button-submit');
+buttonSubmit.addEventListener('click', func);
 let result;
 
 function func(e) {
   e.preventDefault();
-  const number1 = Number(document.getElementById("number1").value);
-  const number2 = Number(document.getElementById("number2").value);
-  const operator = document.getElementById("operator").value;
+  const number1 = Number(document.getElementById('number1').value);
+  const number2 = Number(document.getElementById('number2').value);
+  const operator = document.getElementById('operator').value;
 
   switch (operator) {
-    case "+":
+    case '+':
       result = number1 + number2;
       break;
 
-    case "-":
+    case '-':
       result = number1 - number2;
       break;
 
-    case "*":
+    case '*':
       result = number1 * number2;
       break;
-    case "/":
+    case '/':
       if (number2 === 0 || number1 === 0) {
-        return alert("На ноль делить нельзя!");
+        return alert('На ноль делить нельзя!');
       } else {
         result = number1 / number2;
       }
@@ -31,11 +31,29 @@ function func(e) {
     default:
       break;
   }
-  document.getElementById("result").innerHTML = Math.round(result);
+  document.getElementById('result').innerHTML = Math.round(result);
   clear();
 }
 
 function clear() {
-  document.getElementById("number1").value = "";
-  document.getElementById("number2").value = "";
+  document.getElementById('number1').value = '';
+  document.getElementById('number2').value = '';
 }
+
+// Вызов модального окна
+
+(() => {
+  const refs = {
+    openModalBtn: document.querySelector('[data-modal-open]'),
+    closeModalBtn: document.querySelector('[data-modal-close]'),
+    modal: document.querySelector('[data-modal]'),
+  };
+
+  refs.openModalBtn.addEventListener('click', toggleModal);
+  refs.closeModalBtn.addEventListener('click', toggleModal);
+
+  function toggleModal() {
+    document.body.classList.toggle('modal-open');
+    refs.modal.classList.toggle('is-hidden');
+  }
+})();
